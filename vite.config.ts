cfae92f3d549da95770a1ff44e8ccc5212d3f3b7
weisheng <1780903673@qq.com>
2024-03-23 16:59:14 +0800

    chore: 🚀 更新wot-design-uni@1.2.3并调整组件自动导入方式

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: weisheng
  * @Date: 2023-03-21 22:49:24
- * @LastEditTime: 2023-12-25 22:29:38
+ * @LastEditTime: 2024-03-23 16:37:16
  * @LastEditors: weisheng
  * @Description:
- * @FilePath: \wot-starter-retail\vite.config.ts
+ * @FilePath: /wot-starter-retail/vite.config.ts
  * 记得注释
  */
 import { defineConfig } from 'vite'
@@ -12,9 +12,16 @@ import uni from '@dcloudio/vite-plugin-uni'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 
+import Components from '@uni-helper/vite-plugin-uni-components'
+
+import { WotResolver } from '@uni-helper/vite-plugin-uni-components/resolvers'
+
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/wot-starter-retail/' : './',
   plugins: [
+    Components({
+      resolvers: [WotResolver()]
+    }),
     uni(),
     Unocss(),
     AutoImport({
